740a970fe20afe6c578b7b1ad961e2566576cc16
yatesab <yatesab@gmail.com>
2019-01-03 21:14:53 -0700

    Made Modules on the project

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const http = require('http');
+const module1 = require('./module1');
+const module2 = require('./module2');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Test');
+  res.write(module2.myVariable);
+  module2.myFunction();
   res.end();
 }).listen(8000);
 
